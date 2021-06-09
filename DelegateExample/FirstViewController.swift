408240d51468e0e34a22ca1bd6050f67885287fa
Michel Bernardo <bernardoenglish@outlook.com>
2021-06-09 14:27:17 -0300

    project structure complete.

--- DelegateExample/FirstViewController.swift
+++ DelegateExample/FirstViewController.swift
@@ -8,11 +8,7 @@
 import UIKit
 
 //MARK: 5th Step - Create a struct to conform the transfered object format
-struct HandleTextFields {
-    var nameLabel: String
-    var ageLabel: String
-    var techLabel: String
-}
+
 
 class FirstViewController: UIViewController {
     
@@ -27,16 +23,11 @@ class FirstViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let navigation = segue.destination as? UINavigationController,
            let secondViewController = navigation.topViewController as? SecondViewController {
-            secondViewController.delegate = self
+            //secondViewController.delegate = self
         }
     }
     
     //MARK: 6th Step - Create a function to update the Labels using the transfered object
-    func updateLabels(handleTextFields: HandleTextFields) {
-        nameLabel.text = handleTextFields.nameLabel
-        ageLabel.text = handleTextFields.ageLabel
-        techLabel.text = handleTextFields.techLabel
-    }
-    
+
 }
 
